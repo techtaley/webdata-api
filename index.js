e1d894f002b21ba977129e5c3ee52f2de918a689
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-05-14 18:57:11 -0400

    updated routes

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cors = require("cors")
 app.use(cors())
 
 //routes
-app.get("/", (req, res) => {
-    res.send("Welcome to my REST API. => https://webdata-api.herokuapp.com/data")
-})
+// app.get("/", (req, res) => {
+//     res.send("Welcome to my REST API. => https://webdata-api.herokuapp.com/data")
+// })
 
 app.get("/data", (req, res) => { 
     res.send(importData)
